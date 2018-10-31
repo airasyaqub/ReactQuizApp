3cac71bb4aa81c1ecc44c79cdd5d55d98a3b6aca
Airas Yaqub <airasyaqub6@gmail.com>
2018-11-01 04:56:16 +0500

    Updates

---
+++ precache-manifest.089862a6bdb7d547a66143f6622bca96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ed1acb1ccd7ee77868652da990d00f",
+    "url": "/ReactQuizApp/static/media/js.88ed1acb.png"
+  },
+  {
+    "revision": "87f46e144e5ba6ccfe1df6c76c585395",
+    "url": "/ReactQuizApp/static/media/html.87f46e14.png"
+  },
+  {
+    "revision": "32182cc1729043c1dcf882d7ca2a03ce",
+    "url": "/ReactQuizApp/static/media/css3.32182cc1.png"
+  },
+  {
+    "revision": "1579144672783e820fe2",
+    "url": "/ReactQuizApp/static/js/runtime~main.15791446.js"
+  },
+  {
+    "revision": "dda0665ae7110040e044",
+    "url": "/ReactQuizApp/static/js/main.dda0665a.chunk.js"
+  },
+  {
+    "revision": "943af63ef28f3153961f",
+    "url": "/ReactQuizApp/static/js/1.943af63e.chunk.js"
+  },
+  {
+    "revision": "dda0665ae7110040e044",
+    "url": "/ReactQuizApp/static/css/main.a80733c0.chunk.css"
+  },
+  {
+    "revision": "943af63ef28f3153961f",
+    "url": "/ReactQuizApp/static/css/1.a3643340.chunk.css"
+  },
+  {
+    "revision": "747c544f806c40a4cb7f9ddff130fb08",
+    "url": "/ReactQuizApp/index.html"
+  }
+];
